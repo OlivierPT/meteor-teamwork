47f2296dac90346c0aa0057cfbeaa8024ea08235
Olivier PILLAUD TIRARD <olivier.pillaudtirard@gmail.com>
2014-05-13 21:03:19 +0200

    Ajout des Teams Cleaning

--- server/methods/team.js
+++ server/methods/team.js
@@ -6,20 +6,26 @@ Meteor.methods({
 
   'createTeam': function (name) {
       console.log("MethodCall : createTeam - name = "+name);
-      Team.insert({name: name, creator: Meteor.userId(), {$push: {members: Meteor.userId()});
+      Team.insert({name: name, creator: Meteor.userId(), members: [Meteor.userId()]});
       console.log("Team created");
     },
 
     'addMember': function (teamId, userId) {
-      Team.update({_id: teamId},{$push: {members: userId});
+      console.log("MethodCall : addMember - userId = "+userId);
+      Team.update({_id: teamId},{$push: {members: userId}});
+      console.log("Team memeber added");
     },
 
     'removeMember': function (teamId, userId) {
-      Team.update({_id: teamId},{$pull: {members: userId});
+      console.log("MethodCall : removeMember - userId = "+userId);
+      Team.update({_id: teamId},{$pull: {members: userId}});
+      console.log("Team member removed");
     },
 
     'deleteTeam': function (teamId) {
+      console.log("MethodCall : removeMember - teamId = "+teamId);
       Team.remove({_id: teamId});
+      console.log("Team deleted");
     }
 
 
