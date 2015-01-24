8091588a6babd6c1cc44b9017691a191cdf19e5d
Olivier PILLAUD TIRARD <olivier.pillaudtirard@gmail.com>
2015-01-24 20:31:10 +0100

    Polymer implmentation

--- client/views/main/material_activity/material_activity.js
+++ client/views/main/material_activity/material_activity.js
@@ -2,10 +2,10 @@
 /* MaterialActivity: Event Handlers and Helpersss .js*/
 /*****************************************************************************/
 Template.MaterialActivity.events({
-    'click #new-state-btn': function (e, tmpl) {    
+    'click #new-state-btn': function (e, tmpl) {
         document.querySelector("#new-state-dialog").toggle();
     }
-    
+
 });
 
 Template.MaterialActivity.helpers({
@@ -24,13 +24,13 @@ Template.MaterialActivity.created = function () {
 };
 
 Template.MaterialActivity.rendered = function () {
-    
-    
+
+    // Selector pour la creation d'un activite
     document.querySelector('#add-state-btn').addEventListener('click', function (e) {
-        
-        var stateLabel = this.parentElement.querySelector("#newStateLabel").value;        
+
+        var stateLabel = this.parentElement.querySelector("#newStateLabel").value;
         var activityId = this.getAttribute("data-target-activity-id");
-        
+
         Meteor.call('addState', stateLabel, activityId, function (error, result) {
             // identify the error           
             if (error) {
@@ -39,10 +39,25 @@ Template.MaterialActivity.rendered = function () {
                 emitNotification("State created.");
             }
         });
-        
+
         this.parentElement.querySelector("#newStateLabel").value = "";
     });
-    
+
+    // Selector pour l'evenement de suppression d'un etat
+    document.querySelector('.material-state-list').addEventListener('delete-list', function (e) {
+        console.log(e.type, e.detail.id); 
+        var stateId = e.detail.id;
+        var activityId = "";
+        Meteor.call('removeState', activityId, stateId, function (error, result) {
+            // identify the error           
+            if (error) {
+                emitError("Impossible to delete the state.", error);
+            } else {
+                emitNotification("State deleted.");
+            }
+        });
+    });
+
 };
 
 Template.MaterialActivity.destroyed = function () {
