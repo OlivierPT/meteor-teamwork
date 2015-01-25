2574142bad87421471025b5969690cbac56c214e
Olivier PILLAUD TIRARD <olivier.pillaudtirard@gmail.com>
2015-01-25 23:41:25 +0100

    Polymer implmentation

--- client/views/main/material_activity/material_activity.js
+++ client/views/main/material_activity/material_activity.js
@@ -25,35 +25,34 @@ Template.MaterialActivity.created = function () {
 
 Template.MaterialActivity.rendered = function () {
 
-    // Selector pour la creation d'un activite
-    document.querySelector('#add-state-btn').addEventListener('click', function (e) {
-
-        var stateLabel = this.parentElement.querySelector("#newStateLabel").value;
-        var activityId = this.getAttribute("data-target-activity-id");
-
-        Meteor.call('addState', stateLabel, activityId, function (error, result) {
-            // identify the error           
-            if (error) {
-                emitError("Impossible to create the state.", error);
-            } else {
-                emitNotification("State created.");
-            }
-        });
-
-        this.parentElement.querySelector("#newStateLabel").value = "";
-    });
-
     // Selector pour l'evenement de suppression d'un etat
-    document.querySelector('.material-state-list').addEventListener('delete-list', function (e) {
+    document.querySelector('tw-activity').addEventListener('delete-list', function (e) {
         console.log(e.type, e.detail.id); 
         var stateId = e.detail.id;
-        var activityId = "";
-        Meteor.call('removeState', activityId, stateId, function (error, result) {
+        var activityId = e.detail.activity;
+        Meteor.call('removeState', stateId, activityId, function (error, result) {
             // identify the error           
             if (error) {
                 emitError("Impossible to delete the state.", error);
             } else {
                 emitNotification("State deleted.");
+            }
+        });
+    });
+    
+    // Selector pour l'evenement de suppression d'un etat
+    document.querySelector('tw-activity').addEventListener('add-task', function (e) {
+        console.log(e.type, e.detail.id); 
+        var stateId = e.detail.id;
+        var activityId = e.detail.activity;
+        var taskLabel = e.detail.taskLabel;
+        
+        Meteor.call('addTask', taskLabel, activityId, stateId, function (error, result) {
+            // identify the error           
+            if (error) {
+                emitError("Impossible to add a task.", error);
+            } else {
+                emitNotification("Task added.");
             }
         });
     });
